d43cf93887a3d6db8becb2d2e3dcf5a9f0b61f70
nosovdmitry007 <nosov.dmitry@yahoo.com>
2023-05-15 16:52:53 +0300

    Add dubli and all detection

--- jacket/dubli.py
+++ jacket/dubli.py
@@ -4,40 +4,42 @@ from PIL import Image
 import imagehash
 import pandas as pd
 
-df = pd.DataFrame()
-put = './Данные для обучения нейросети/классификация/dataset/baza/Hat'
-path = os.listdir(put)
-k = path.copy()
+def dubli(put):
+    df = pd.DataFrame()
+    path = os.listdir(put)
+    k = path.copy()
 
-for i in path:
-    k.remove(i)
-    slov = {}
-    filename = []
-    hashes = []
-    otherhashes = []
-    hamming_distance = []
-    fil_h = []
-    # print(len(k))
-    for j in k:
-        hash = imagehash.average_hash(Image.open(f'{put}/{i}'))
-        otherhash = imagehash.average_hash(Image.open(f'{put}/{j}'))
+    for i in path:
+        k.remove(i)
+        slov = {}
+        filename = []
+        hashes = []
+        otherhashes = []
+        hamming_distance = []
+        fil_h = []
+        # print(len(k))
+        for j in k:
+            hash = imagehash.average_hash(Image.open(f'{put}/{i}'))
+            otherhash = imagehash.average_hash(Image.open(f'{put}/{j}'))
 
-        hamming_distances = hash - otherhash  # hamming distance
-        if hamming_distances < 8:
-            filename.append(i)
-            fil_h.append(j)
-            hashes.append(str(hash))
-            otherhashes.append(str(otherhash))
-            hamming_distance.append(hamming_distances)
+            hamming_distances = hash - otherhash  # hamming distance
+            if hamming_distances < 8:
+                filename.append(i)
+                fil_h.append(j)
+                hashes.append(str(hash))
+                otherhashes.append(str(otherhash))
+                hamming_distance.append(hamming_distances)
 
-    slov['filename'] = filename
-    slov['filename_out'] = fil_h
-    slov['hash'] = hashes
-    slov['otherhash'] = otherhashes
-    slov['hamming_distance'] = hamming_distance
-    df_dictionary = pd.DataFrame(slov)
-    df = pd.concat([df, df_dictionary], ignore_index=True)
-    # print(df)
-    df.drop_duplicates(subset=['filename_out'])
-print(df.filename_out.drop_duplicates())
-df.to_csv('./Данные для обучения нейросети/классификация/dataset/baza/dubli_hat.csv', encoding='utf-8')
+        slov['filename'] = filename
+        slov['filename_out'] = fil_h
+        slov['hash'] = hashes
+        slov['otherhash'] = otherhashes
+        slov['hamming_distance'] = hamming_distance
+        df_dictionary = pd.DataFrame(slov)
+        df = pd.concat([df, df_dictionary], ignore_index=True)
+        # print(df)
+        df.drop_duplicates(subset=['filename_out'])
+    print(df.filename_out.drop_duplicates())
+    df.to_csv('./Данные для обучения нейросети/классификация/dataset/baza/dubli_hat.csv', encoding='utf-8')
+
+dubli('./Данные для обучения нейросети/классификация/dataset/baza/Hat')
